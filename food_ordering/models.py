4f50591673d2fb0fb9e683e0d1cdd52823cc4860
Chibuzor Prince <chibuzorprnce68@gmail.com>
2025-02-13 01:53:38 -0800

    Your commit message

--- food_ordering/models.py
+++ food_ordering/models.py
@@ -17,7 +17,7 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
-    g
+    
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100, blank=True, null=True)  # ✅ Add this if missing
@@ -25,8 +25,7 @@ class UserProfile(models.Model):
     profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True, null=True, default="profile_pictures/default_profile.jpg")
 
     def __str__(self):
-         return self.username  # ✅ Correct attribute
-
+        return self.user.usernameclear
 
 
 class Order(models.Model):
